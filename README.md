aa108b98e8d59f34789f4f61182af649c5ad8b06
Alexandre Velloso <xande.velloso@gmail.com>
2024-10-23 18:11:54 +0100

    Add timelapse script

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # py-timelapse
 Python scripts to generate video timelapse from a sequence of photos
+
+## Install dependencies
+
+### (Optional) Create virtual environment
+Create virtual environment
+```
+python -m venv env
+```
+
+Activate it
+```
+source env/bin/activate
+```
+
+## Install project dependencies
+
+Run the pip to install all dependencies listed in the requirements.txt folder
+
+```
+pip install -r requirements.txt
+```
+
+## Run
+
+To run the project you can use the main python script named timelapse.py
+
+```
+python timelapse.py
+```
---
+++ timelapse.py
@@ -0,0 +1,30 @@
+import cv2
+import os
+
+folder = 'photos'
+files = [f for f in sorted(os.listdir(folder)) if not f.startswith('.') and not f.startswith('README')]
+
+print(files)
+exit(0)
+
+img_array = []
+
+for filename in files:
+    path = os.path.join('.', folder, filename)
+
+    try:
+        img = cv2.imread(path)
+
+        height, width, layers = img.shape
+        size = (width,height)
+
+        img_array.append(img)
+    except Exception:
+        print('Error in path: ' + path)
+
+out = cv2.VideoWriter(folder + '.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 20.0, size)
+
+for i in range(len(img_array)):
+   out.write(img_array[i])
+
+out.release()
